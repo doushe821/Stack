5bf8195eae4cb77933f4397c518de1cc73031e53
doushe821 <romachevskii.ae@phystech.edu>
2024-09-25 19:06:41 +0300

    F version 2

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include "stack.h"
+int main()
+{
+
+}
+
+stackInit()
+{
+
+}
+
+stackDestroy(stack* st)
+{
+
+}
+
+dump()
+{
+
+}
+
+stackOK()
+{
+
+}
+
+stackError()
+{
+
+}
+
+stackResize()
+{
+
+}
+
+push()
+{
+
+}
+
+int pop(stack* st, int* el)
+{
+
+}
+
+struct stack
+{
+
+};
---
+++ stack.h
@@ -0,0 +1,9 @@
+#ifndef STACK_H_INCLUDED
+#define STACK_H_INCLUDED
+
+struct stack_t
+{
+    stack Element* data;
+};
+
+#endif
