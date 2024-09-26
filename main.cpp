c514ebfc61e09ae8ed12f4c9834c6575446a18cb
doushe821 <romachevskii.ae@phystech.edu>
2024-09-26 16:37:37 +0300

    123321

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <assert.h>
 #include <errno.h>
 
-#define MVHS fprintf(stderr, "Mushroom Wizard Honey Sack") 
+#define MVHS fprintf(stderr, "Mushroom Wizard Honey Sack\n") 
 
 typedef double StackElem_t;
 
@@ -44,9 +44,9 @@ int main()
 {
     Stack_t stk = {};
 
-    StackInit(&stk, 20);
+    StackInit(&stk, 67);
 
-    for(int i = 0; i < 20; i++)
+    for(int i = 0; i < 65; i++)
     {
        
         StackPush(&stk, 69.69);
@@ -57,7 +57,7 @@ int main()
     fprintf(stderr, "%zu\n", stk.capacity);
     double x = 0;
     StackPop(&stk, &x);
-    fprintf(stderr, "%zu", stk.size);
+    fprintf(stderr, "%zu\n", stk.size);
 
     MVHS;
 
@@ -71,14 +71,15 @@ int main()
 
 int StackDtor(Stack_t* stk)
 {
+    memset(stk->data, 0, sizeof(StackElem_t)*stk->capacity);
     free(stk->data);
     return 0; 
 }
+
 int StackPush(Stack_t* stk, StackElem_t elem)
 {
     if(stk->size >= stk->capacity)
     {
-        fprintf(stderr, "shadow wizard money gang\n");
         if(StackResize(stk, false) != 0)
         {
             assert(0);
@@ -118,24 +119,30 @@ int StackResize(Stack_t* stk, bool downSizeFlag)
     {
         if(wrecalloc(stk, stk->capacity/REALLOC_COEF, sizeof(StackElem_t)) == NULL)
         {
+            perror("Wrecalloc:");
+            free(stk->data);
             assert(0);
+            
             return ALLOC_ERROR;
         }
+        stk->capacity /= REALLOC_COEF;
         return 0;
     }
 
     else
     {
-        if(wrecalloc(stk, stk->capacity*REALLOC_COEF, sizeof(StackElem_t)) == NULL)
+        if(wrecalloc(stk->data, stk->capacity*REALLOC_COEF, sizeof(StackElem_t)) == NULL)
         {
             perror("Wrecalloc:");
             free(stk->data);
             assert(0);
+            
             return ALLOC_ERROR;
         }
+        fprintf(stderr, "REALL DONE\n");
+        stk->capacity *= REALLOC_COEF;
         return 0;
     } 
-
     return 0;
 }
 
@@ -145,6 +152,7 @@ int StackInit(Stack_t* stk, size_t InitCapacity)
     if((stk->data = (double*)calloc(16, sizeof(double))) == NULL)
     {
         fprintf(stderr, "Dynamic Memory dead\n");
+        assert(0);
         return ALLOC_ERROR; 
     }
     stk->size = 0;
@@ -154,6 +162,5 @@ int StackInit(Stack_t* stk, size_t InitCapacity)
 
 void* wrecalloc(void* ptr, size_t num, size_t size)
 {
-    fprintf(stderr, "shadow wizard money gang\n");
     return realloc(ptr, num*size);//memset(, 0, num*size);
 } 
